52da0987d707e464abc149fe39b17f894eab7745
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-03-02 23:44:13 +0000

    teste

--- numero100.java
+++ numero100.java
@@ -1,7 +1,7 @@
 public class numero100 {
     public static void main (String[] args) {
         int contador = -100;
-        while (contador <= 1)
+        while (contador < 2)
         {
             System.out.println(contador);
             contador++;
