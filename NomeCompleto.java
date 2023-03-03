2f9a7050cd12a5043533b4b9e3cb90da26717d7d
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-03-03 01:22:34 +0000

    teste

--- NomeCompleto.java
+++ NomeCompleto.java
@@ -2,7 +2,7 @@ public class NomeCompleto {
     public static void main(String[] args){
     
     String nomeCompleto = "Mariane Lopes";
-        for (int i=0; i < nomeCompleto.lenght(); i++){
+        for (int i=0; i < nomeCompleto.length(); i++){
             System.out.print(nomeCompleto.charAt(i));
         }
         
